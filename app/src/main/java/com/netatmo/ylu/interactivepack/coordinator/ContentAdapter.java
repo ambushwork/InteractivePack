8279b655cd2741cf1b399cbeb8b522bc7622680e
ambushwork <378248322@qq.com>
2019-06-27 06:18:47 +0800

    [Coordinator + AppBarLayout] Moving toolbar and sticky header

---
+++ app/src/main/java/com/netatmo/ylu/interactivepack/coordinator/ContentAdapter.java
@@ -0,0 +1,64 @@
+package com.netatmo.ylu.interactivepack.coordinator;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.netatmo.ylu.interactivepack.R;
+import com.squareup.picasso.Picasso;
+
+public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.FeedHolder> {
+    @NonNull
+    @Override
+    public FeedHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_coordinator_list, viewGroup, false);
+        return new FeedHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedHolder feedHolder, int i) {
+        Picasso.get()
+                .load(this.getAvatarUrl(i))
+                .centerInside()
+                .fit()
+                .into(feedHolder.mIvAvatar);
+        Picasso.get()
+                .load(this.getContentUrl(i))
+                .centerInside()
+                .fit()
+                .into(feedHolder.mIvContent);
+
+        feedHolder.mTvNickname.setText("Person " + i);
+    }
+
+    private String getAvatarUrl(int position) {
+        return "https://i.pravatar.cc/150?img=" + position;
+    }
+
+    private String getContentUrl(int position) {
+        return String.format("https://picsum.photos/id/%s/1920/1080", String.valueOf(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+    static class FeedHolder extends RecyclerView.ViewHolder {
+
+        final ImageView mIvAvatar;
+        final ImageView mIvContent;
+        final TextView mTvNickname;
+
+        FeedHolder(@NonNull View itemView) {
+            super(itemView);
+            mIvAvatar = itemView.findViewById(R.id.item_coordinator_avatar);
+            mIvContent = itemView.findViewById(R.id.item_coordinator_preview);
+            mTvNickname = itemView.findViewById(R.id.item_coordinator_name);
+        }
+    }
+
+}
