bd4abbd5e1c6f6b9a17726425c2abd7a9d8fd4e2
ambushwork <378248322@qq.com>
2019-07-07 15:11:07 +0800

    wiP

--- app/src/main/java/com/netatmo/ylu/interactivepack/vectoranimation/SecurityView.java
+++ app/src/main/java/com/netatmo/ylu/interactivepack/vectoranimation/SecurityView.java
@@ -1,10 +1,13 @@
 package com.netatmo.ylu.interactivepack.vectoranimation;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.core.graphics.PathParser;
@@ -24,8 +27,12 @@ import java.util.List;
 
 public class SecurityView extends View {
 
-    List<Path> paths = new ArrayList<>();
-    Paint paint = new Paint();
+    private final List<PathUnit> pathUnits = new ArrayList<>();
+    private final Paint paint = new Paint();
+    private final Path dst = new Path();
+
+
+    private float mAnimatedValue;
 
     public SecurityView(Context context) {
         super(context);
@@ -59,7 +66,7 @@ public class SecurityView extends View {
             for (int i = 0; i < items.getLength(); i++) {
                 Element element = (Element) items.item(i);
                 String pathData = element.getAttribute("android:pathData");
-                paths.add(PathParser.createPathFromPathData(pathData));
+                pathUnits.add(new PathUnit(PathParser.createPathFromPathData(pathData)));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -68,19 +75,58 @@ public class SecurityView extends View {
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         }
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f, 1f);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mAnimatedValue = (Float) animation.getAnimatedValue();
+                SecurityView.this.invalidate();
+            }
+        });
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.setDuration(3000);
+        valueAnimator.start();
+    }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d("onMeasure", String.format("Width %s, Height %s", MeasureSpec.getSize(widthMeasureSpec),
+                MeasureSpec.getSize(heightMeasureSpec)));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
+        canvas.scale(10.0f, 10.0f);
         paint.setColor(this.getResources().getColor(R.color.colorAccent));
+        paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
-        for (Path path : paths) {
-            canvas.drawPath(path, paint);
+
+        for (PathUnit unit : pathUnits) {
+            float start = 0f;
+            float distance = mAnimatedValue * unit.mLength;
+            unit.pathMeasure.getSegment(start, distance, dst, true);
+            canvas.drawPath(dst, paint);
+            dst.reset();
+
+            //canvas.drawPath(path, paint);
         }
         canvas.restore();
-
     }
+
+
+    private class PathUnit {
+        private final PathMeasure pathMeasure;
+        private final float mLength;
+        private final Path path;
+
+        public PathUnit(Path path) {
+            this.path = path;
+            pathMeasure = new PathMeasure(path, true);
+            mLength = pathMeasure.getLength();
+        }
+    }
+
 }
